22967cd8aed4959a6b1621d4a8be4a40f6e3a198
Noah Manion <noah@Noahs-MacBook-Pro.local>
2019-09-22 15:05:27 -0500

    strengthened palindrome, tests pass. ready for 9

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ function Phrase(content) {
 	
 	// Returns content processed for palindrome testing.
 	this.processedContent = function processedContent() { 
-		return this.content.toLowerCase();
+		return this.letters().toLowerCase();
 	}
+	
+	this.letters = function letters() {
+		//return some kind of regex that removes punctuations
+		return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("")
+	}
+
 	// Returns true if the phrase is a palindrome, false otherwise.
 	this.palindrome = function palindrome() {
 		return this.processedContent() === this.processedContent().reverse();
--- test/test.js
+++ test/test.js
@@ -13,5 +13,20 @@ describe("Phrase", function () {
 			let plainPalindrome = new Phrase("racecar");
 			assert(plainPalindrome.palindrome());
 		});
+
+		it("should return true for a mixed-case palindrome", function () {
+			let mixedCase = new Phrase("RaceCar");
+			assert(mixedCase.palindrome());
+		});
+
+		it("should remove punctuation by using letters", function () {
+			let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+			assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+		})
+
+		it("should return true for a palindrome with punctuation", function () {
+			let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+			assert(punctuatedPalindrome.palindrome());
+		})
 	});
 });
